bf7e276f12c14c49233cf72bf1399d791a552659
osbornetunde <osbornetunde@gmail.com>
2021-07-30 23:13:41 +0100

    added swagger docs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
+const path = require('path')
 const express = require('express');
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-
+const swaggerDocPath = path.resolve(__dirname, './swagger.yml')
+const swaggerDoc = YAML.load(swaggerDocPath)
 
 const app = express();
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
 
 
 
